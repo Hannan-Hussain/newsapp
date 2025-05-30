7342c33a9cc109ec7d2b2c1cbe2a08313ecfce1d
Hannan Hussain <hh2371502@gmail.com>
2025-05-30 05:06:31 -0700

    tags

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:newsapp/screens/headlines_screen.dart';
+import 'package:newsapp/screens/tag_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -33,7 +34,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HeadlinesScreen(),
+      home: const TagsScreen(),
     );
   }
 }
